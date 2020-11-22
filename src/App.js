47e4e5d94ba902b3184a208cd1968827239cd2f5
surajsharma <surajsharma@pm.me>
2020-11-22 19:30:11 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import "./style.css";
 const NUM_OPTIONS = 3;
 
 export default function App() {
-  let [excel, setExcel] = useState(null);
-  let [cells, setCells] = useState(null);
   let [msgs, setMsgs] = useState([]);
-  let [messagesSet, setMessagesSet] = useState(false);
+  let [excel, setExcel] = useState(null);
 
   useEffect(async () => {
     const response = await fetch(
@@ -15,56 +13,47 @@ export default function App() {
     );
 
     const data = await response.json();
+    const cells = data.feed.entry.map(e => e.gs$cell);
+    let message = { content: "", options: [], actions: [] };
+    let messages = [];
+    let questions = [],
+      options = [],
+      actions = [];
+
+    cells &&
+      cells.forEach((c, index) => {
+        if (index == 0 || index % 7 === 0) {
+          questions.push(c.$t);
+        } else {
+          if (c.col == 2) {
+            actions.push(c.$t);
+          } else {
+            options.push(c.$t);
+          }
+        }
+      });
+
+    questions &&
+      questions.forEach((q, index) => {
+        message.content = q;
+        let sliceStarts = index === 0 ? 0 : NUM_OPTIONS * index;
+        let sliceEnds = sliceStarts + NUM_OPTIONS;
+        message.options = options.slice(sliceStarts, sliceEnds);
+        message.actions = actions.slice(sliceStarts, sliceEnds);
+        messages.push(message);
+      });
+
+    if (messages.length) {
+      setMsgs(messages);
+    }
     setExcel(data);
   }, []);
-
-  useEffect(() => {
-    if (excel) {
-      const newCells = excel.feed.entry.map(e => e.gs$cell);
-      setCells(newCells);
-    }
-  }, [excel]);
-
-  useEffect(() => {
-    if (!messagesSet) {
-      let message = { content: "", options: [], actions: [] };
-      let messages = [];
-      let questions = [],
-        options = [],
-        actions = [];
-
-      cells &&
-        cells.forEach((c, index) => {
-          if (index == 0 || index % 7 === 0) {
-            questions.push(c.$t);
-          } else {
-            if (c.col == 2) {
-              actions.push(c.$t);
-            } else {
-              options.push(c.$t);
-            }
-          }
-        });
-
-      questions &&
-        questions.forEach((q, index) => {
-          message.content = q;
-          let sliceStarts = index === 0 ? 0 : NUM_OPTIONS * index;
-          let sliceEnds = sliceStarts + NUM_OPTIONS;
-          message.options = options.slice(sliceStarts, sliceEnds);
-          message.actions = actions.slice(sliceStarts, sliceEnds);
-          messages.push(message);
-        });
-      setMsgs(messages);
-      setMessagesSet(true);
-    }
-  }, [cells]);
 
   const handleClickOption = () => {};
 
   return (
     <div>
-      <h1 onClick={() => console.log(visitor)}>
+      <h1 onClick={() => console.log(msgs)}>
         Chat Bot From Google Sheets {excel && excel.version}
       </h1>
     </div>
